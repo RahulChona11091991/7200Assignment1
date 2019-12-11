4cc8b6f4d723f4a47197cef4a223178405f7b4c3
Mac Mojave <rahulchona11@gmail.com>
2019-12-11 13:42:13 -0700

    Added Code to create tsv files.

--- Question1.py
+++ Question1.py
@@ -12,8 +12,23 @@ with open ('./allPosts-metadata.tsv', 'w+') as output:
     output.write('user_id\tquestion_id\tpost_type\tanswer_id\n')
 
 
+#Assignment 2 - Start
+with open ('./ABAN.tsv', 'w+') as output:
+    output.write('askerId\tbestAnswererId\tquestionId\tbestAnswerId\tmaxUpvoteCount\n')
+
+with open ('./CBEN.tsv', 'w+') as output:
+    output.write('nonBestAnswererId\tbestAnswererId\tquestionId\tbestAnswerId\tmaxUpvoteCount\n')
+
+with open ('./VBEN.tsv', 'w+') as output:
+    output.write('nonBestAnswererId\tbestAnswererId\tanswerersEdgeWeight\tquestionId\tbestAnswerId\tmaxUpvoteCount\n')
+
+with open ('./VBEN2.tsv', 'w+') as output:
+    output.write('askerId\tanswererId\taskerAnswererEdgeWeight\tnonBestAnswererId\tbestAnswererId\taskerAnswererEdgeWeight\tquestionId\tbestAnswerId\tmaxUpvoteCount\n')
+
+#Assignment 2 - Complete
+
 # Fetch Questions and answers
-for index in range(10):
+for index in range(5):
     url = 'https://api.stackexchange.com//2.2/questions?page={}&pagesize=100&fromdate=1514764800&todate=1569801600&order=desc&sort=activity&tagged=python&site=stackoverflow&filter=!BHrIhNpdLSrQDA*mgWaaqI0fLxig0y'.format(index+1)
 
     print('URL: {}'.format(url))
@@ -24,7 +39,22 @@ for index in range(10):
 
         # check if answer exists, then execute
         if i2['answer_count'] > 0:
+
+            maxUpvoteCount = 0
+            askerId = ''
+            bestAnswererId = ''
+            questionId = ''
+            bestAnswerId = ''
+
             for answerItem in i2['answers']:
+
+                if maxUpvoteCount < answerItem['up_vote_count']:
+                    maxUpvoteCount = answerItem['up_vote_count']
+                    questionId = answerItem['question_id']
+                    askerId = i2['owner']['user_id'] if i2['owner']['user_type'] == 'registered' else i2['owner']['display_name']
+                    bestAnswerId = answerItem['answer_id']
+                    bestAnswererId = answerItem['owner']['user_id'] if answerItem['owner']['user_type'] == 'registered' else (answerItem['owner']['display_name'] + str(i2['question_id']))
+
 
                 with open('./askerAnswerer.tsv', 'a+') as output:
                     output.write('{}\t{}\t{}\n'.format(
@@ -64,6 +94,63 @@ for index in range(10):
                         is_accepted,
                         'na', # 'na' represents not applicable.
                     ))
+            if maxUpvoteCount > 0:
+                with open('./ABAN.tsv', 'a+') as output:
+                    output.write('{}\t{}\t{}\t{}\t{}\n'.format(
+                        askerId,
+                        bestAnswererId,
+                        questionId,
+                        bestAnswerId,
+                        maxUpvoteCount
+                ))
+
+                for answerItem in i2['answers']:
+                    nonBestAnswererId = answerItem['owner']['user_id'] if answerItem['owner']['user_type'] == 'registered' else (answerItem['owner']['display_name'] + str(i2['question_id']))
+                    if bestAnswererId != nonBestAnswererId :
+                        with open('./CBEN.tsv', 'a+') as output:
+                            output.write('{}\t{}\t{}\t{}\t{}\n'.format(
+                                nonBestAnswererId,
+                                bestAnswererId,
+                                questionId,
+                                bestAnswerId,
+                                maxUpvoteCount
+                        ))
+                        nonBestAnswerVoteCount = answerItem['up_vote_count']
+                        edgeWeight = float(float(nonBestAnswerVoteCount)/float(maxUpvoteCount))
+                        # print edgeWeight
+                        with open('./VBEN.tsv', 'a+') as output:
+                            output.write('{}\t{}\t{}\t{}\t{}\t{}\n'.format(
+                                nonBestAnswererId,
+                                bestAnswererId,
+                                edgeWeight,
+                                questionId,
+                                bestAnswerId,
+                                maxUpvoteCount
+                        ))
+
+                    answererId = answerItem['owner']['user_id'] if answerItem['owner']['user_type'] == 'registered' else (answerItem['owner']['display_name'] + str(i2['question_id']))
+                    questionVoteCount = i2['up_vote_count']
+                    answerVoteCount = answerItem['up_vote_count']
+                    upvoteDiffBtwQuestionAnswer = 0.0
+                    upvoteDiffBtwQuestionAnswer = answerVoteCount - questionVoteCount
+
+                    if upvoteDiffBtwQuestionAnswer > 0:
+                        print upvoteDiffBtwQuestionAnswer
+                        with open('./VBEN2.tsv', 'a+') as output:
+                            output.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(
+                                askerId,
+                                answererId,
+                                upvoteDiffBtwQuestionAnswer,
+                                nonBestAnswererId,
+                                bestAnswererId,
+                                edgeWeight,
+                                questionId,
+                                bestAnswerId,
+                                maxUpvoteCount
+                            ))
+
+
+
 
         # Question
         is_answered = 'no'
